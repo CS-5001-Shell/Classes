2469c137d29ce80004649614f17aee636dd87d2b
Sami Rollins <srollins@gmail.com>
2024-11-11 19:01:37 -0800

    clarify doc; fix style formatting

--- student.py
+++ student.py
@@ -1,47 +1,49 @@
 class Student:
-	"""
-	Data about a single student.
-	"""
-	
-	def __init__(self, name, id, grades=None):
-		"""Constructor
-		Parameters:
-		  - name: student's name
-		  - id: student's id
-		  - grades: optional list of scores		
-		"""
-		pass
+    """
+    Data about a single student.
+    """
 
-	def add_grade(self, grade):
-		"""Append a new score onto the grades list.
-		"""
-		pass
+    def __init__(self, name: str, id: int, grades=None):
+        """Constructor
+        Parameters:
+          - name: student's name
+          - id: student's id
+          - grades: optional list of scores
+        """
+        pass
 
-	def get_average(self):
-		"""Return the mean across all scores in the grades list.
-		"""
-		pass
+    def add_grade(self, grade: float):
+        """Append a new score onto the grades list.
+        """
+        pass
 
-	def get_letter_grade(self):
-		"""Return the mapping from average score to letter grade.
-		Greater than or equal to 90 is an A.
-		Greater than or equal to 80 is a B.
-		Greater than or equal to 70 is a C.
-		All other scores earn an F.
-		"""
-		pass
+    def get_average(self) -> float:
+        """Return the mean across all scores in the grades list.
+        Return 0 if the student does not have grades.
+        """
+        pass
 
-	def __str__(self):
-		"""Returns a string representation of the Student.
-		Note: in order for this to work correctly you must have
-		an instance variable named name that is a string, and 
-		instance variable named id that is an int, and an
-		instance variable named grades that is a list of float.
-		"""
-		result = f'\t{self.name} - {self.id}:\n\t\t'
-		for grade in self.grades:
-			result += str(grade) + ' '
-		result += '\n'
-		result += f'\t\tAverage: {self.get_average()} - {self.get_letter_grade()}'
-		result += '\n'
-		return result
+    def get_letter_grade(self) -> str:
+        """Return the mapping from average score to letter grade.
+        Greater than or equal to 90 is an A.
+        Greater than or equal to 80 is a B.
+        Greater than or equal to 70 is a C.
+        All other scores earn an F.
+        """
+        pass
+
+    def __str__(self):
+        """Returns a string representation of the Student.
+        Note: in order for this to work correctly you must have
+        an instance variable named name that is a string, and
+        instance variable named id that is an int, and an
+        instance variable named grades that is a list of float.
+        """
+        result = f'\t{self.name} - {self.id}:\n\t\t'
+        for grade in self.grades:
+            result += str(grade) + ' '
+        result += '\n'
+        result += f'\t\tAverage: {self.get_average()}'
+        result += ' - {self.get_letter_grade()}'
+        result += '\n'
+        return result
--- student_list.py
+++ student_list.py
@@ -1,63 +1,64 @@
 from student import Student
 
+
 class Student_List:
-	"""
-	Data about a class of students
-	"""
+    """
+    Data about a class of students
+    """
 
-	def __init__(self, course, students=None):
-		"""Constructor		
-		Parameters:
-		  - course: string description of course (e.g., CS 5001)
-		  - students: optional dictionary of id -> Student mapping		
-		"""
-		pass
+    def __init__(self, course: str, students=None):
+        """Constructor
+        Parameters:
+          - course: string description of course (e.g., CS 5001)
+          - students: optional dictionary of id -> Student mapping
+        """
+        pass
 
-	def add_student(self, student):
-		"""Add a new student to the class.			
-		Parameters:
-		  - student: Student object containing student information		
-		Raises an exception if student.id already exists in the 
-		student list.
-		"""
-		pass
+    def add_student(self, student: Student):
+        """Add a new student to the class.
+        Parameters:
+          - student: Student object containing student information
+        Raises an exception if student.id already exists in the
+        student list.
+        """
+        pass
 
-	def update_student(self, student_id, new_grade):
-		"""Updates a student's grade list with a new score.
-		Parameters:
-		  - student_id: int representing the id of the student
-		  - new_grade: float representing the new score
-		Raises an exception if student.id does not exist in the 
-		student list.
-		"""
-		pass
+    def update_student(self, student_id: int, new_grade: float):
+        """Updates a student's grade list by adding a new score.
+        Parameters:
+          - student_id: int representing the id of the student
+          - new_grade: float representing the new score
+        Raises an exception if student.id does not exist in the
+        student list.
+        """
+        pass
 
-	def get_student_average(self, student_id):
-		"""Returns a student's average grade.
-		Parameters:
-		  - student_id: int representing the id of the student
-		Raises an exception if student.id does not exist in the 
-		class.
-		"""
-		pass
+    def get_student_average(self, student_id: int) -> float:
+        """Returns a student's average grade.
+        Parameters:
+          - student_id: int representing the id of the student
+        Raises an exception if student.id does not exist in the
+        class.
+        """
+        pass
 
-	def get_student_letter_grade(self, student_id):
-		"""Returns a student's letter grade.
-		Parameters:
-		  - student_id: int representing the id of the student
-		Raises an exception if student.id does not exist in the 
-		class.
-		"""
-		pass
+    def get_student_letter_grade(self, student_id: int) -> str:
+        """Returns a student's letter grade.
+        Parameters:
+          - student_id: int representing the id of the student
+        Raises an exception if student.id does not exist in the
+        class.
+        """
+        pass
 
-	def __str__(self):
-		"""Returns a string representation of the class.
-		Note: in oder for this to work correctly you must have
-		an instance variable named course that is a string and 
-		an instance variable named students that is a dictionary
-		mapping int to Student.		
-		"""
-		result = f'{self.course}\n'
-		for student in self.students.values():
-			result += student.__str__()
-		return result
+    def __str__(self):
+        """Returns a string representation of the class.
+        Note: in oder for this to work correctly you must have
+        an instance variable named course that is a string and
+        an instance variable named students that is a dictionary
+        mapping int to Student.
+        """
+        result = f'{self.course}\n'
+        for student in self.students.values():
+            result += student.__str__()
+        return result
